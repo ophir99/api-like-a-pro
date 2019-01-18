df61249b2953488b72e8eed36c6dd0e8c5c0632b
kavya.pl <kavya.pl@gmail.com>
2019-01-18 10:27:15 +0530

    xtndd

--- controllers/posts.js
+++ controllers/posts.js
@@ -27,3 +27,30 @@ exports.createPost = (req, res) => {
       res.status(500).send({ response: err });
     });
 };
+
+exports.updatePost = (req, res) => {
+  post
+    .update(
+      { _id: req.params.id },
+      {
+        $set: {
+          title: req.body.title,
+          desc: req.body.desc,
+          hashtags: req.body.hashtags
+        }
+      }
+    )
+    .then(result => {
+      res.send({ response: result });
+    })
+    .catch(err => {
+      res.status(500).send({ response: err });
+    });
+};
+
+exports.deletePost = (req, res) => {
+  post
+    .deleteOne({ _id: req.params.id })
+    .then(result => res.send({ response: result }))
+    .catch(err => res.status(500).send({ response: err }));
+};
--- routes/posts.js
+++ routes/posts.js
@@ -2,4 +2,6 @@ const router = require("express").Router();
 const posts = require("./../controllers/posts");
 router.get("/all", posts.getAllPosts);
 router.post("/new", posts.createPost);
+router.put("/update/:id", posts.updatePost);
+router.delete("/:id", posts.deletePost);
 module.exports = router;
